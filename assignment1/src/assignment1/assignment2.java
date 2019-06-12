e46ace5300209b0aa0d0851144dc386c0139548f
Julian Olsen <46683927+JulianOlsen@users.noreply.github.com>
2019-06-12 17:48:40 +0200

    Add files via upload

---
+++ assignment1/src/assignment1/assignment2.java
@@ -0,0 +1,29 @@
+package assignment2;
+import java.util.Scanner;
+ 
+public class assignment2
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner (System.in);
+        Scanner scane = new Scanner (System.in);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        String think;
+        String am;
+        System.out.println("First word");
+        think = scan.next();
+        System.out.println("second word");
+        am = scane.next();
+        System.out.println("I "+think+", therefore I "+am);
+        
+
+    }   
+}
+
